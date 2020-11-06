0ec47d3fecdb33e367e2701319e1db776a0412b4
jhonny.rohr@gmail.com <jhonny.rohr@gmail.com>
2020-11-06 15:48:43 -0300

    Incluido e configurando o carrosel de fotos

---
+++ CloneNetflix/js/owl/setup.js
@@ -0,0 +1,17 @@
+// Configurações das imagens que vamos utilizar no carrosel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
